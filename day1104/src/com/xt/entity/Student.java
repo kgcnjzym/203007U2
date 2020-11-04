3fae0a874aa2ea2270198ef67126eac20653798f
kgcnjzym <kgcnjzym@sohu.com>
2020-11-05 00:47:01 +0800

    2020-11-04 课堂示例

---
+++ day1104/src/com/xt/entity/Student.java
@@ -0,0 +1,17 @@
+package com.xt.entity;
+
+import lombok.Data;
+
+/**
+ * @author 杨卫兵
+ * @version V1.00
+ * @date 2020/11/4 11:13
+ * @since V1.00
+ */
+@Data
+public class Student {
+    private Integer id;
+    private Integer age;
+    private String name ;
+    private String gender;
+}
